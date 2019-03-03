595481b8028276ff3f008cc44e67286969c548fe
andron13 <andron13@gmail.com>
2019-03-03 17:04:43 +0100

    try to create build with maven command line. not worked

--- src/main/java/de/BotTokenReader.java
+++ src/main/java/de/BotTokenReader.java
@@ -1,3 +1,5 @@
+package de;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
--- src/main/java/de/Main.java
+++ src/main/java/de/Main.java
@@ -1,3 +1,5 @@
+package de;
+
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
